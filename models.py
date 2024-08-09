6fc5cb83abf853e79a0cc97a7da6d8801ff8dc9f
Marcelo Fernandes <marcelo.fernandes@dce.ufpb.br>
2024-08-09 11:21:57 -0300

    Commit 151

--- models.py
+++ models.py
@@ -224,7 +224,7 @@ class MQTTClient():
         self.client.username_pw_set(self.username, self.password)
         while not self.connected:
             try:
-                self.client.connect(self.broker, self.port, 6000)
+                self.client.connect(self.broker, self.port, 6)
                 self.connected = True
             except ConnectionRefusedError as e:
                 logger.info(f"Erro de conexão: {e}. Tentando reconectar em 5 segundos...")
